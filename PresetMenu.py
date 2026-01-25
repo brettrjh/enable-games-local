4eb57282c77116d7507e5251d80c3dfe64126479
kaigor911 <112411651+kaigor911@users.noreply.github.com>
2026-01-25 16:22:57 -0500

    Combined All Menus
    This commit has all the most updated menus from everyone except for physical. Brett, the update you made to the .ui file for physical two days ago breaks your menu (keeps it from opening, specifically) when I try to use that one instead of your previous one (note that I renamed your previous one from PhysMenu.ui to eg_physical_settings.ui to keep the naming convention the same across all ui files).
    
    Also added the preset to download, download to preset, and preset to main window swapping for the Preset and Download Preset classes. Changed the name of the .ui files for preset menu and download too so that they match the naming convention.

--- PresetMenu.py
+++ PresetMenu.py
@@ -1,13 +1,10 @@
 import os
 from PyQt6 import QtWidgets, uic
-
 from PyQt6.QtGui import QPixmap
-
-from PyQt6.QtCore import QPoint
-from PyQt6.QtGui import QAction
 
 # needed for connectivity/opening the main menu
 import MainMenu
+
 
 # ------------------------------------------------------------------------------
 # PresetMenu Class: 
@@ -21,27 +18,138 @@ class PresetMenu(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         
-        # Loads the UI file and sets the window title
-        uic.loadUi('ui files/eg_presets_menu.ui', self)
+        # for later, helps with getting files as for some reason the previous code here did not work for me (Leo)
+        baseDir = os.path.dirname(__file__)
+
+        # Loads the UI file and sets the window title ---> these 3 lines were stolen from visual menu code, but I used a variable
+        ui_path = os.path.join(baseDir, "ui files", "eg_presets_menu.ui")
+        uic.loadUi(ui_path, self)
         self.setWindowTitle('Preset Menu')
 
+        # Loads the icon dynamically
+        img_path = os.path.join(baseDir, "ui files", "Images", "preset_filter_icon.jpg")
+        pixmap = QPixmap(img_path)
+        if pixmap.isNull():
+            print("could not load icon", img_path)
+        self.lblImage.setPixmap(pixmap)
+        self.lblImage.setScaledContents(True)
+
         # Connections of button click events to specific functions
-        self.pushButton.clicked.connect(self.on_button_click)
-        self.pushButton_2.clicked.connect(self.on_button_click_2)
+        self.btnFilter.clicked.connect(self.on_btnFilter_click)
+        self.btnPresetToggle.clicked.connect(self.on_btnPresetToggle_click)
+        self.btnDownloads.clicked.connect(self.on_btnDownloads_click)
+        self.btnBackToMain.clicked.connect(self.on_btnBackToMain_click)
 
     # ------------------------------------------------------------
-    # [insert name] Function:
+    # Filter Options click Function:
     # - called when the given button is clicked
-    def on_button_click(self):
-        print("Button from the UI was clicked!")
+    def on_btnFilter_click(self):
+        print("Filter button was clicked") # remove this later
 
     # ------------------------------------------------------------
-    # [insert name] Function:
+    # Toggle Presets click Function:
     # - called when the given button is clicked
-    def on_button_click_2(self):
-        self.label.setText('Hello World')
+    def on_btnPresetToggle_click(self):
+        print("Toggle preset button was clicked") # remove this later
+
+    # ------------------------------------------------------------
+    # Downloads Button click Function:
+    # - called when the given button is clicked
+    def on_btnDownloads_click(self):
+        try:
+            self.downloadW = PresetDownload()
+            self.downloadW.show()
+            self.close()
+        except Exception as e:
+            print(f"error: {e}")
+            import traceback
+            traceback.print_exc()
+
+    # ------------------------------------------------------------
+    # Back Button click Function:
+    # - called when the given button is clicked
+    def on_btnBackToMain_click(self):
+        # return to main menu
+        print("Back to main button clicked")
+        try:
+            print('creating main menu...')
+            self.mainW = MainMenu.MainMenu()
+            print('showing main menu...')
+            self.mainW.show()
+            print('closing audio menu...')
+            self.close()
+            print('done!')
+        except Exception as e:
+            print(f"error: {e}")
+            import traceback
+            traceback.print_exc()
 
 
+
+
+# ------------------------------------------------------------------------------
+# PresetDownload Class: 
+# - inherits from the QWidget
+# ------------------------------------------------------------------------------
+class PresetDownload(QtWidgets.QWidget):
+    # -------------------------------------------------------------
+    # Initialization for ui file and connecting buttons to functions
+    def __init__(self):
+        super().__init__()
+        
+        # for later, helps with getting files
+        baseDir = os.path.dirname(__file__)
+
+        # Loads the UI file and sets the window title
+        ui_path = os.path.join(baseDir, "ui files", "eg_preset_download.ui") # add file name here
+        uic.loadUi(ui_path, self)
+        self.setWindowTitle('Preset Download')
+
+        # Loads the icon dynamically
+        img_path = os.path.join(baseDir, "ui files", "Images", "preset_download_icon.png")
+        pixmap = QPixmap(img_path)
+        if pixmap.isNull():
+            print("could not load icon", img_path)
+        self.lblImage.setPixmap(pixmap)
+        self.lblImage.setScaledContents(True)
+
+        # Connections of button click events to specific functions
+        self.btnPresetDownload.clicked.connect(self.on_btnPresetDownload_click)
+        self.btnCreator.clicked.connect(self.on_btnCreator_click)
+        self.btnReport.clicked.connect(self.on_btnReport_click)
+        self.btnBackToPreset.clicked.connect(self.on_btnBackToPreset_click)
+
+    # ------------------------------------------------------------
+    # Preset Download click Function:
+    # - called when the given button is clicked
+    def on_btnPresetDownload_click(self):
+        print("Download preset was clicked") # remove this later
+    
+    # ------------------------------------------------------------
+    # Creator click Function:
+    # - called when the given button is clicked
+    def on_btnCreator_click(self):
+        print("More from this creator was clicked") # remove this later
+
+
+    # ------------------------------------------------------------
+    # Report click Function:
+    # - called when the given button is clicked
+    def on_btnReport_click(self):
+        print("Report button was clicked") # remove this later
+
+    # ------------------------------------------------------------
+    # Back Button click Function:
+    # - called when the given button is clicked
+    def on_btnBackToPreset_click(self):
+        try:
+            self.presetW = PresetMenu()
+            self.presetW.show()
+            self.close()
+        except Exception as e:
+            print(f"error: {e}")
+            import traceback
+            traceback.print_exc()
 
 
 # ---------------------------------------------------------------------------------
@@ -51,6 +159,14 @@ if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)  # Create the application
-    presetW = PresetMenu()                  # Create an instance of the PresetMenu class
-    presetW.show()                          # Show the preset menu
+
+    # -------------------------------------------------------------------
+    # uncomment which menu you want to test here
+
+    presetMenuW = PresetMenu()
+    presetMenuW.show()
+
+    #presetDownloadW = PresetDownload()
+    #presetDownloadW.show()
+
     sys.exit(app.exec())                    # Run the application's event loop
